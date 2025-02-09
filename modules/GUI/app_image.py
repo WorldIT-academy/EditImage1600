e0424917b40e9735dc062017cab98827415ec028
WorldIT-academy <worldit_academy@worldit.academy>
2025-02-09 17:42:34 +0200

    Create show image

--- modules/GUI/app_image.py
+++ modules/GUI/app_image.py
@@ -1,10 +1,11 @@
 import customtkinter as ctk
 import PIL.Image
+
 class ImageLabel(ctk.CTkLabel):
     
     def __init__(self, ch_master: ctk.CTkFrame = None, path_image: str = None, **kwargs):
-        self.WIDTH = ch_master._current_width
-        self.HEIGHT = ch_master._current_height
+        self.WIDTH = int(ch_master._current_width * 0.9)
+        self.HEIGHT = int(ch_master._current_height * 0.9)
         
         self.PATH_IMAGE = path_image
         ctk.CTkLabel.__init__(
@@ -19,8 +20,9 @@ class ImageLabel(ctk.CTkLabel):
         try:
             image = PIL.Image.open(fp = self.PATH_IMAGE)
             return ctk.CTkImage(
-                light_image= PIL.Image.open(fp = image),
-                size= (image.width, image.height) if image.width < self.WIDTH and image.height < self.HEIGHT else (self.WIDTH, self.HEIGHT)
+                light_image= image,
+                size = self.image_size(image = image)
+                # size= (image.width, image.height) if image.width < self.WIDTH and image.height < self.HEIGHT else (self.WIDTH, self.HEIGHT)
                 # if image.width < self.WIDTH and image.height < self.HEIGHT:
                 #     return (image.width, image.height)
                 # else:
@@ -29,10 +31,16 @@ class ImageLabel(ctk.CTkLabel):
         except Exception as error:
             print(f"Error while loading image: {str(error)}")
             return None
+    def image_size(self, image: PIL.Image):
+        if image.width == image.height:
+            if image.width >= self.WIDTH and image.height >= self.HEIGHT:
+                return (self.HEIGHT, self.HEIGHT)
+            else:
+                return(image.width, image.height)
+        elif image.width >= self.WIDTH and image.height >= self.HEIGHT:
+            return (self.WIDTH, self.HEIGHT)
+        else:
+            return (image.width, image.height)
+        
 
-def show_image(parent: ctk.CTkFrame, path_image: str | None):
-    image_label = ImageLabel(ch_master= parent, path_image= path_image)
-    image_label.pack()
 
-
-# image = ImageLabel(path_image= "/Users/worlditacademy/Downloads/worldIT_LOGO.ico").load_image()
--- modules/tools/search_image.py
+++ modules/tools/search_image.py
@@ -2,9 +2,21 @@ import tkinter.filedialog as filedialog
 from ..gui.app_button import AppButton
 from tkinter import Y, BOTH, LEFT, TOP
 import customtkinter as ctk
-from ..gui.app_image import show_image
+from ..gui.app_image import ImageLabel
 
 
+list_images = []
+
+def show_image(button_name: str, frame: ctk.CTkFrame):
+    list_frame = frame.winfo_children()
+    for child in list_frame:
+        if isinstance(child, ImageLabel):
+            child.pack_forget()
+
+    for image in list_images:
+        if image.PATH_IMAGE.split('/')[-1] == button_name:
+            image.pack(anchor = "center")
+            
 def get_file_path(parent: ctk.CTk, button_parent: ctk.CTkScrollableFrame | ctk.CTkFrame, dashboard: ctk.CTkFrame):
     r'''
         Визначає шлях до вибраного файлу зображення і створює кнопку з вибраним ім'ям файла.
@@ -30,8 +42,15 @@ def get_file_path(parent: ctk.CTk, button_parent: ctk.CTkScrollableFrame | ctk.C
         button = AppButton(
             ch_master = button_parent,
             text_button = path.split('/')[-1],
-            function= lambda: show_image(parent= dashboard, path_image= path)
+            function = lambda name_image = path.split('/')[-1]: show_image(button_name = name_image, frame = dashboard)
+            # function= lambda: s
+            # how_image(parent= dashboard, path_image= path)
         )
         button.pack(pady = 20, padx = 20, anchor = 'w')
+        image = ImageLabel(
+            ch_master = dashboard,
+            path_image = path   
+        )
+        list_images.append(image)
     print() 
     
